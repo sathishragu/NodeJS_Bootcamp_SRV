113e6b78c68e5e98a891e1c3f135fc172b5be9ec
sathishragu <srathin@L-SNVN1RQDV7-M.local>
2015-04-23 21:37:59 -0700

    Made changes to implement loglevel
    log level is implemented to print appropriate logs only if log level
    arg is passed

--- index.js
+++ index.js
@@ -5,17 +5,25 @@ let argv = require('yargs')
 	.argv
 let fs = require('fs')
 let through = require('through')
+let log = require('loglevel')
 
 let scheme = 'http://'
 let port = argv.port || argv.host === '127.0.0.1' ? 8000 : 80
 let destinationUrl = argv.url || scheme + argv.host + ':' + port
 let logFileStream = argv.logfilename ? fs.createWriteStream(argv.logfilename) : process.stdout
 let testvar = argv.logfilename
+let logLevel = argv.loglevel || 'info'
+
+log.setLevel(logLevel)
 
 
 
 
 http.createServer((req, res) => {
+	log.info('\nINFO : Testing loglevel')
+    log.warn('\nWARN : Testing loglevel')
+    log.error('\nERROR : Testing loglevel')
+    log.trace('\nTRACE : Testing loglevel')
     logFileStream.write('\n\nEcho Req: \n' + JSON.stringify(req.headers))
     for(let header in req.headers){
     	res.setHeader(header,req.headers[header])
